9a18eb1fddfcc7964fb8af5d260ec16281568e22
navinbarnwal <knbarnwal@gmail.com>
2021-05-06 23:13:05 +0530

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelect} from '@angular/material/select';
+import {RouterModule} from '@angular/router';
+import {MatSelectModule} from '@angular/material/select';
+
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import {config} from 'rxjs';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    RouterModule,
+    MatSelectModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyBRoevaxPr7l5khB_ZBh8gONZSZ91KL2qM',
+    authDomain: 'jivrus-9a769.firebaseapp.com',
+    databaseURL: 'https://jivrus-9a769-default-rtdb.firebaseio.com',
+    projectId: 'jivrus-9a769',
+    storageBucket: 'jivrus-9a769.appspot.com',
+    messagingSenderId: '335536434269',
+    appId: '1:335536434269:web:8b0abbc57a17031e4f31b4',
+    measurementId: 'G-1TC64HDVP4'
+  }
 };
